ba676d083cdb9f7f6135b20b98726bf00a07985f
AlexSimenko <simenkoalex2@gmail.com>
2022-12-31 21:19:03 +0200

    Test1

---
+++ src/GeolacationCastle.java
@@ -0,0 +1,12 @@
+public class GeolacationCastle {
+    public static void main(String[] args) {
+        double Latitude= 54.38;
+        double Longitude= 24.56;
+        castle(Latitude,Longitude);
+    }
+    static void castle(double x, double y){
+        System.out.println("Trakai is a castle  is located by "+x+
+                " north latitude"+" and "+y+" east longitude");
+    }
+
+}
